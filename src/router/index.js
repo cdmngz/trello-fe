6969105a60ed22799b63d0022d1a77bb198a1dc3
cdmngz <crlsdmngz3@gmail.com>
2020-07-09 11:36:55 +0200

    Auth en rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store/index'
 
 Vue.use(VueRouter)
 
@@ -7,12 +8,15 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue')
+    component: () => import('../views/Home.vue'),
+    meta: { requiresAuth: false }
+
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: () => import('../views/Dashboard.vue')
+    component: () => import('../views/Dashboard.vue'),
+    meta: { requiresAuth: true }
   }
 ]
 
@@ -20,6 +24,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const reqAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isAuth = store.state.token
+  if(reqAuth && !isAuth) {
+    next('/')
+  } else if(reqAuth===false && isAuth) {
+    next('/dashboard')
+  } else {
+    next()
+  }
 })
 
 export default router
